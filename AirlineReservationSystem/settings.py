435bc634a2bb5df9395aba9115653e230f94aa39
soumoks <soumoks@gmail.com>
2020-03-16 18:25:09 -0600

    fixed syntax issues

--- AirlineReservationSystem/settings.py
+++ AirlineReservationSystem/settings.py
@@ -47,7 +47,7 @@ AWS_LOCATION = 'static'
 AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-static-files'
 AWS_S3_REGION_NAME = 'us-east-1'
 # Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
