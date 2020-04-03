cc10cb41bf6b67d376885e02a6a708c51f0f4de0
soumoks <soumoks@gmail.com>
2020-04-02 21:13:41 -0600

    Fixed serializer for trip

--- airlineapp/serializers.py
+++ airlineapp/serializers.py
@@ -4,20 +4,21 @@ from rest_framework import serializers
 class RouterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Route
-        fields = '__all__'
+        fields = ['departure_city','arrival_city']
 
 
 class PlaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Plane
-        fields = '__all__'
+        fields = ['company','model_no','capacity','max_row','max_col']
 
 class TripSerializer(serializers.ModelSerializer):
-    routes = RouterSerializer(many=True,read_only=True)
-    planes = PlaneSerializer(many=True,read_only=True)
+    #use the same name as that of the column name in Trip
+    route_id = RouterSerializer(read_only=True)
+    plane_id = PlaneSerializer(read_only=True)
     class Meta:
         model = models.Trip
-        fields = ['date','arrival_time','departure_time','routes','planes']
+        fields = ['date','arrival_time','departure_time','route_id','plane_id']
 
 class FoodNameSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,9 +26,9 @@ class FoodNameSerializer(serializers.ModelSerializer):
         fields = ['food_name']
 
 class PassengerSerializer(serializers.ModelSerializer):
-    food = FoodNameSerializer(read_only=True)
-    # food_name = serializers.RelatedField(source='food', read_only=True)
+    #use the same name as that of the column name in Food_Name
+    food_name = FoodNameSerializer(read_only=True)
     class Meta:
         model = models.Passenger
-        fields = ['fname','lname','age','sex','seat_number','food']
+        fields = ['fname','lname','age','sex','seat_number','food_name']
 
--- airlineapp/views.py
+++ airlineapp/views.py
@@ -1,30 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Trip,Food_Name,Route,Passenger
+from .models import Trip,Food_Name,Route,Passenger,Plane
 from rest_framework import viewsets
 from rest_framework import permissions
-from airlineapp.serializers import TripSerializer,FoodNameSerializer,RouterSerializer,PassengerSerializer
+from airlineapp.serializers import TripSerializer,FoodNameSerializer,RouterSerializer,PassengerSerializer,PlaneSerializer
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to the Airline Application!")
 
 #API endpoint for REST framework
-
-class TripViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint for Trips
-    """
-    
-    queryset = Trip.objects.all()
-    serializer_class = TripSerializer(queryset,many=True)
-        
-
-class FoodViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint for FoodNames
-    """
-    queryset = Food_Name.objects.all()
-    serializer_class = FoodNameSerializer(queryset,many=True)
 
 class RouterViewSet(viewsets.ModelViewSet):
     """
@@ -32,6 +16,30 @@ class RouterViewSet(viewsets.ModelViewSet):
     """
     queryset = Route.objects.all()
     serializer_class = RouterSerializer
+
+class PlaneViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for Planes
+    """
+    queryset = Plane.objects.all()
+    serializer_class = PlaneSerializer
+
+class TripViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for Trips
+    """
+    
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+        
+
+class FoodViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for FoodNames
+    """
+    queryset = Food_Name.objects.all()
+    serializer_class = FoodNameSerializer
+
 
 class PassengerViewSet(viewsets.ModelViewSet):
     """
