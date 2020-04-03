4bb0286be49693dd4f845c9b395ab399cd18463c
soumoks <soumoks@gmail.com>
2020-04-03 14:51:29 -0600

    removed repeated middleware

--- AirlineReservationSystem/settings.py
+++ AirlineReservationSystem/settings.py
@@ -69,7 +69,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
